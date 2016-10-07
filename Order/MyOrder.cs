78107a1e9a07fd2564944d331b5fb28ed03b82a9
guest0307 <guest0307@GUEST0307-PC>
2016-10-07 13:52:42 +0800

    加入專案檔案。

---
+++ Order/MyOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace Order
+{
+    public class MyOrder
+    {
+        public int id { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
+    }
+}
---
+++ Order/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Order
+{
+    public class Product
+    {
+        public List<int> GetOrderSum(List<MyOrder> myOrders, int groupNum,string type)
+        {
+            List<int> result = new List<int>();
+            int groupNow = 0;
+            List<int> costGroup = null;
+            if (type == "Cost")
+            {
+                costGroup = myOrders.Select(t => t.Cost).ToList();
+            }
+            else if (type == "Revenue")
+            {
+                costGroup = myOrders.Select(t => t.Revenue).ToList();
+            }
+  
+            
+            for (int i = 0; i < costGroup.Count(); i++)
+            {
+                if (i % groupNum == 0)
+                {
+                    result.Add(0);
+                    groupNow++;
+                };
+                result[groupNow-1] += costGroup[i];
+            }
+            return result;
+        }
+    }
+}
