804104016ab8efe4992d790af66b40b1e80618e4
guest0307 <guest0307@GUEST0307-PC>
2016-10-11 11:58:42 +0800

    修改寫法

--- OrderTest/UnitTest1.cs
+++ OrderTest/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Order;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OrderTest
 {
@@ -16,7 +17,7 @@ namespace OrderTest
             Product target = new Product();
             List<int> expected = new List<int>() {6,15,24,21};
 
-            List<int> actual = target.GetOrderSum(myOrders, 3, "Cost");
+            List<int> actual = target.GetOrderSum(myOrders, 3, t => t.Cost).ToList();
 
             CollectionAssert.AreEqual(expected, actual);
         }
@@ -29,25 +30,28 @@ namespace OrderTest
             Product target = new Product();
             List<int> expected = new List<int>() { 50, 66, 60};
 
-            List<int> actual = target.GetOrderSum(myOrders, 4, "Revenue");
+            List<int> actual = target.GetOrderSum(myOrders, 4, t=> t.Revenue).ToList();
 
             CollectionAssert.AreEqual(expected, actual);
         }
 
         private List<MyOrder> GetProduct()
         {
-            List<MyOrder> products = new List<MyOrder>();
-            products.Add(new MyOrder() { id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
-            products.Add(new MyOrder() { id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
-            products.Add(new MyOrder() { id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
-            products.Add(new MyOrder() { id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
-            products.Add(new MyOrder() { id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
-            products.Add(new MyOrder() { id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
-            products.Add(new MyOrder() { id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
-            products.Add(new MyOrder() { id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
-            products.Add(new MyOrder() { id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
-            products.Add(new MyOrder() { id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
-            products.Add(new MyOrder() { id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
+            List<MyOrder> products = new List<MyOrder>()
+            {
+                new MyOrder() { id = 1, Cost = 1, Revenue = 11, SellPrice = 21 },
+                new MyOrder() { id = 2, Cost = 2, Revenue = 12, SellPrice = 22 },
+                new MyOrder() { id = 3, Cost = 3, Revenue = 13, SellPrice = 23 },
+                new MyOrder() { id = 4, Cost = 4, Revenue = 14, SellPrice = 24 },
+                new MyOrder() { id = 5, Cost = 5, Revenue = 15, SellPrice = 25 },
+                new MyOrder() { id = 6, Cost = 6, Revenue = 16, SellPrice = 26 },
+                new MyOrder() { id = 7, Cost = 7, Revenue = 17, SellPrice = 27 },
+                new MyOrder() { id = 8, Cost = 8, Revenue = 18, SellPrice = 28 },
+                new MyOrder() { id = 9, Cost = 9, Revenue = 19, SellPrice = 29 },
+                new MyOrder() { id = 10, Cost = 10, Revenue = 20, SellPrice = 30 },
+                new MyOrder() { id = 11, Cost = 11, Revenue = 21, SellPrice = 31 }
+            };
+
             return products;
         }
     }
